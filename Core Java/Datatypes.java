375d3345ff3a82949c1e66b9f1b397d21506741d
deekshithabari-22 <deekshithabari004@gmail.com>
2026-05-29 19:04:09 +0530

    Exercise 6: Datatype Demonstration

---
+++ Core Java/Datatypes.java
@@ -0,0 +1,13 @@
+public class Datatypes {
+    public static void main(String[] args) {
+        int a = 10; // integer data type
+        double b = 3.14; // double data type
+        char c = 'A'; // character data type
+        boolean d = true; // boolean data type
+
+        System.out.println("Integer value: " + a);
+        System.out.println("Double value: " + b);
+        System.out.println("Character value: " + c);
+        System.out.println("Boolean value: " + d);
+    }
+}
