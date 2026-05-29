84c1ba9a19536382659774b1d32a6ca202113a8a
deekshithabari-22 <deekshithabari004@gmail.com>
2026-05-29 19:55:04 +0530

    Exercise 13: Recursive Fibonacci

---
+++ Core Java/RecursiveFibonacci.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+public class RecursiveFibonacci {
+    private static int getNthFibNum(int n){
+        if(n<=2) return n-1;
+        return getNthFibNum(n-1)+getNthFibNum(n-2);
+    }
+   public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter a positive integer n, to get nth fibonacci number:");
+
+        int n=sc.nextInt();
+        int fibNum=getNthFibNum(n);
+
+        System.out.println("The "+n+"th Fibonacci number is: "+fibNum);
+   } 
+}
