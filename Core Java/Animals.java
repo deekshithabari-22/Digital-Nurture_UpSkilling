6d3eb432be4be11eebc30c1dca0974b93f55c316
deekshithabari-22 <deekshithabari004@gmail.com>
2026-05-29 23:13:05 +0530

    Exercise 18: Inheritance Example

---
+++ Core Java/Animals.java
@@ -0,0 +1,9 @@
+public class Animals {
+    public static void main(String[] args) {
+        Dog dog = new Dog();
+        dog.makeSound(); // Output: Bark
+
+        Animal animal = new Animal();
+        animal.makeSound(); // Output: Animal makes a sound
+    }
+}
