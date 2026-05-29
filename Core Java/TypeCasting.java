17c055a30e963e49574978dea5381d70e08224b3
deekshithabari-22 <deekshithabari004@gmail.com>
2026-05-29 19:07:51 +0530

    Exercise 7: TypeCasting Example

---
+++ Core Java/TypeCasting.java
@@ -0,0 +1,12 @@
+public class TypeCasting {
+    public static void main(String[] args){
+        double value=10;
+        int castedValue=(int)value;
+
+        int val=15;
+        double castedVal=val;
+
+        System.out.println(value+" "+castedValue);
+        System.out.println(val+" "+castedVal);
+    }
+}
