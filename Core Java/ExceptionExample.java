387bda039f752bed3e3bd435275d2125e3954fd7
deekshithabari-22 <deekshithabari004@gmail.com>
2026-06-02 19:10:49 +0530

    Exercise 20: Try-Catch Example

---
+++ Core Java/ExceptionExample.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+public class ExceptionExample {
+   public static void main(String[] args){
+    Scanner sc=new Scanner(System.in);
+    System.out.println("Enter two numbers:");
+    int a=sc.nextInt();
+    int b=sc.nextInt();
+    try{
+        int result=a/b;
+        System.out.println("Result: "+result);
+    }
+    catch(ArithmeticException e){
+        System.out.println("Error: Cannot divide by zero.");
+    }
+    finally{
+        sc.close();
+    }
+   } 
+}
